20759076117a8fa0817fbd20dcdfd3a4b34c1769
“Astrolithia” <astrolithia@icloud.com>
2025-02-10 11:03:19 +0800

    用户登录接口

--- mall-backend/src/main/java/com/qvtu/mallbackend/controller/UserController.java
+++ mall-backend/src/main/java/com/qvtu/mallbackend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.qvtu.mallbackend.controller;
 import com.qvtu.mallbackend.pojo.Result;
 import com.qvtu.mallbackend.pojo.User;
 import com.qvtu.mallbackend.service.UserService;
+import com.qvtu.mallbackend.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -32,5 +33,23 @@ public class UserController {
             // 用户名被占用
             return Result.error("用户名已被占用");
         }
+    }
+
+    @PostMapping("/login")
+    public Result<String> login(@Pattern(regexp = "^\\S{2,10}$") String username,
+                                @Pattern(regexp = "^\\S{8,16}$")String password) {
+        // 根据用户名查询用户
+        User loginUser = userService.findByUserName(username);
+        // 判断该用户是否存在
+        if (loginUser == null) {
+            return Result.error("用户名错误");
+        }
+
+        // 判断密码是否正确 loginUser对象中的password是密文
+        if (Md5Util.getMD5String(password).equals(loginUser.getPassword())) {
+            // 登录成功
+            return Result.success("jwt token令牌");
+        }
+        return Result.error("密码错误");
     }
 }

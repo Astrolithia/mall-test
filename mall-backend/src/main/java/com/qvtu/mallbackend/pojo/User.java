7e67c74f6653ac5d369747d726d196ee4a0d9c8e
“Astrolithia” <astrolithia@icloud.com>
2025-02-10 12:04:33 +0800

    更新用户基本信息校验

--- mall-backend/src/main/java/com/qvtu/mallbackend/pojo/User.java
+++ mall-backend/src/main/java/com/qvtu/mallbackend/pojo/User.java
@@ -1,7 +1,11 @@
 package com.qvtu.mallbackend.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.util.Date;
 
@@ -16,8 +20,12 @@ public class User {
     // JsonIgnore注解让Springmvc把当前对象转换为json字符串时忽略这个值
     private String password;
 
+    @NotEmpty
+    @Pattern(regexp = "^\\S{1,10}$")
     private String nickname;
 
+    @NotEmpty
+    @Email
     private String email;
 
     private String phone;
@@ -33,4 +41,6 @@ public class User {
     private Integer createUser;
 
     private Integer updateUser;
+
+    private String avatarUrl;
 }

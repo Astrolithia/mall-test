6528016a830b668c012047675396ca590c1bd148
“Astrolithia” <astrolithia@icloud.com>
2025-02-10 10:58:54 +0800

    用户注册接口增加参数校验

--- mall-backend/src/main/java/com/qvtu/mallbackend/controller/UserController.java
+++ mall-backend/src/main/java/com/qvtu/mallbackend/controller/UserController.java
@@ -3,20 +3,24 @@ package com.qvtu.mallbackend.controller;
 import com.qvtu.mallbackend.pojo.Result;
 import com.qvtu.mallbackend.pojo.User;
 import com.qvtu.mallbackend.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username, String password, String email,String phone) {
+    public Result register(@Pattern(regexp = "^\\S{2,10}$") String username,
+                           @Pattern(regexp = "^\\S{8,16}$")String password, String email, String phone) {
         // 查询用户
         User u = userService.findByUserName(username);
         if (u == null) {
